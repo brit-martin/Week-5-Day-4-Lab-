a7e771798a6d206abaa3d69195b622c1871ce0dc
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2024-01-05 15:06:33 -0700

    make recipe_ingredients, recipe, and author model, as well as foreign keys connection

--- models/model.js
+++ models/model.js
@@ -29,15 +29,84 @@ Ingredient.init(
 
 class Recipe extends Model {}
 
-Recipe.init({},
-    
-    
-    
-    
-    
+Recipe.init(
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        name: {
+            type: DataTypes.STRING(60), 
+            allowNull: false, 
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        instructions: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    },
+    {
+        modelName: 'recipe',
+        sequelize: sequelize,
+        timestamps: false,
     })
+
+class RecipeIngredient extends Model {}
+
+RecipeIngredient.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        quantity: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+        },
+    },
+    {
+        modelName: 'recipe_ingredients',
+        sequelize: sequelize,
+        timestamps: false,
+    })
+
+class Author extends Model {}
+
+Author.init(
+    {
+        id: {
+            type: DataTypes.INTEGER, 
+            autoIncrement: true,
+            primaryKey: true,
+         },
+         firstName: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+         },
+         lastName: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+         },
+    }, 
+    {
+        modelName: 'author',
+        sequelize: sequelize,
+        timestamps: false,
+    })
+
+Recipe.hasMany(RecipeIngredient, {foreignKey: 'recipeId'});
+RecipeIngredient.belongsTo(Recipe, {foreignKey: 'recipeId'});
+
+Author.hasMany(Recipe, {foreignKey: 'authorId'});
+Recipe.belongsTo(Author, {foreignKey: 'authorId'});
+
+Ingredient.hasMany(RecipeIngredient, {foreignKey: 'ingredientId'});
+RecipeIngredient.belongsTo(Ingredient, {foreignKey: 'ingredientId'});
+
 
 await sequelize.sync({ force: true })
 await sequelize.close()
